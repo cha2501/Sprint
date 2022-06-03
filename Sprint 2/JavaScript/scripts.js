44b09606fd8c77eba9f662268031cca6ee498a4a
cha2501 <96985594+cha2501@users.noreply.github.com>
2022-06-03 22:47:11 +0800

    Add files via upload

--- Sprint 2/JavaScript/scripts.js
+++ Sprint 2/JavaScript/scripts.js
@@ -1,17 +1,20 @@
 const start = document.querySelector('.controls button[name="start"]'),
       cards = document.querySelectorAll('.memory-card'),
-      scoreBoard = document.querySelector('.controls .score span');
+      scoreBoard = document.querySelector('.controls .score span')
+      highScoreList = document.querySelector('#highScores')
+      score = 0,
+      NO_OF_HIGH_SCORES = 10
+      highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard, playTime;
-let score = 0;
+
 
 start.addEventListener('click', startGame);
 
-
 function startGame() {
-  playTime = 50;
+  playTime = 10;
   scoreBoard.textContent = 0;
   score = 0;
 
@@ -23,8 +26,11 @@ function scorePoint() {
   score+=2;
   scoreBoard.textContent = score;
 
-  if(score == 12){
+  if(score === 12){
+    checkHighScore(score);
+    showHighScores();
     clearInterval(interval);
+    cards.forEach(card => card.addEventListener('click', flipCard));
   }
 }
 
@@ -33,7 +39,10 @@ function countdown() {
 
   interval = setInterval(() => {
     if (playTime < 0) {
+      checkHighScore(score);
+      showHighScores();
       clearInterval(interval);
+      cards.forEach(card => card.addEventListener('click', flipCard));
       return;
     }
 
@@ -83,6 +92,7 @@ function disableCards() {
   resetBoard();
 }
 
+
 function unflipCards() {
   lockBoard = true;
 
@@ -99,10 +109,31 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
+function showHighScores() {
+  highScoreList.innerHTML = highScores.map((score) => `<li>${score.score} - ${score.name}`).join('');
+}
+
+function checkHighScore(score) {
+  const lowestScore = highScores[NO_OF_HIGH_SCORES - 1]?.score ?? 0;
+
+  if (score > lowestScore) {
+    const name = prompt('You got a highscore! Enter name:');
+    const newScore = { score, name };
+    saveHighScore(newScore, highScores);
+    showHighScores();
+  }
+}
+
+function saveHighScore(score, highScores) {
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(NO_OF_HIGH_SCORES);
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+}
+
 (function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
+  cards.forEach(card => {let randomPos = Math.floor(Math.random() * 12);card.style.order = randomPos;});
 })();
+
 
